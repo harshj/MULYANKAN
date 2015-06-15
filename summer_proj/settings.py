2e11d723fe6b0e4cdb5303bae563dbd2a66a5d1a
Harsh Jain <harshjain1194@gmail.com>
2015-06-16 01:07:55 +0530

    1. Added license. 2. GUI improvements for home.

--- summer_proj/settings.py
+++ summer_proj/settings.py
@@ -1,3 +1,4 @@
+import os
 # Django settings for summer_proj project.
 
 DEBUG = True
@@ -61,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '../static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -69,6 +70,8 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+        'static',          
+        #os.path.join(os.getcwd() , '/static/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -121,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'summer_proj',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
