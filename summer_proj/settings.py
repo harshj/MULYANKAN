f3901ebd651dd4a8f3dec187fa24af8e42a08672
Harsh Jain <harshjain1194@gmail.com>
2015-08-11 13:35:57 +0530

    1. Minor GUI improvements. 2. Added download button for .xls files.

--- summer_proj/settings.py
+++ summer_proj/settings.py
@@ -71,6 +71,7 @@ STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
         'static',          
+		'system/data',
         #os.path.join(os.getcwd() , '/static/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
